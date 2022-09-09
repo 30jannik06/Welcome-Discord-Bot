a5d0e130a592a9b35460b62e8d8a829b03e22b4a
30jannik06 <80638124+30jannik06@users.noreply.github.com>
2022-09-09 08:51:48 +0200

    [ADDED] LogHelper
    Sends better looking messages to console when something gets logged.

---
+++ src/util/logHelper.ts
@@ -0,0 +1,50 @@
+/*
+ * Class created by .jannik
+ * https://www.github.com/30jannik06
+ * 09.09.22, 08:28
+ */
+/*
+ * Needed Version of Chalk:
+ * chalk@4.1.2
+ */
+import chalk from "chalk";
+class LogHelper {
+    async log(msg: string) {
+        /*
+         * Sends normal Log Message to Console
+         */
+        console.log(chalk.bgYellow("📗[BOT]") + " " + chalk.white(msg));
+    }
+    async error(msg: string) {
+        /*
+         * Sends error Message to Console
+         */
+        console.error(chalk.bgRed("📕[ERROR]") + " " + chalk.white(msg));
+    }
+    async warn(msg: string) {
+        /*
+         * Sends warn Message to Console
+         */
+        console.warn(chalk.bgCyan("📙[WARN]") + " " + chalk.white(msg));
+    }
+    async cmd(msg: string) {
+        /*
+         * Sends "Command" tagged log to Console
+         */
+        console.log(chalk.bgMagenta("📘[CMD]") + " " + chalk.white(msg));
+    }
+    async evnt(msg: string) {
+        /*
+         * Sends "Event" tagged log to Console
+         */
+        console.log(chalk.bgGray("📓[EVENT]") + " " + chalk.white(msg));
+    }
+    /*
+     * Clear's the Console.
+     */
+    async clear() {
+        console.clear();
+    }
+}
+
+export const { log, error, warn, cmd, evnt, clear } = new LogHelper();
